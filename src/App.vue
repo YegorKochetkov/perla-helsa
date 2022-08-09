37397e33df88f3799083d17dfb1c915184d87748
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-10 02:36:44 +0300

    Add Gilroy font

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,47 @@
 </template>
 
 <style lang="scss">
+/* Regular */
+@font-face {
+    font-family: "Gilroy";
+    src: local("Gilroy"),
+         url("~@/fonts/gilroy-regular.ttf") format("truetype"),
+         url("~@/fonts/gilroy-regular.eot?#iefix") format("embedded-opentype"),
+         url("~@/fonts/gilroy-regular.woff") format("woff");
+}
+
+/* SemiBold */
+@font-face {
+    font-family: "GilroyMedium";
+    src: local("Gilroy"),
+         url("~@/fonts/gilroy-medium.ttf") format("truetype"),
+         url("~@/fonts/gilroy-medium.eot?#iefix") format("embedded-opentype"),
+         url("~@/fonts/gilroy-medium.woff") format("woff");
+}
+
+/* Bold */
+@font-face {
+    font-family: "GilroyBold";
+    src: local("Gilroy"),
+         url("~@/fonts/gilroy-bold.ttf") format("truetype"),
+         url("~@/fonts/gilroy-bold.eot?#iefix") format("embedded-opentype"),
+         url("~@/fonts/gilroy-bold.woff") format("woff");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Gilroy, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #00284F;
 }
 
 nav {
   padding: 30px;
 
   a {
-    font-weight: bold;
+    // font-weight: bold;
+    font-family: GilroyMedium;
     color: #2c3e50;
 
     &.router-link-exact-active {
