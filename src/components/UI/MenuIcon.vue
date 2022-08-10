5b05899d50a14feb7ac47d6fbf60dc83f5c35ed7
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-10 23:09:31 +0300

    Add menu icon

---
+++ src/components/UI/MenuIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div role="button" class="nav-icon">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/styles/utils/variables.scss";
+@import "@/styles/utils/mixins.scss";
+
+.nav-icon {
+  margin: 0;
+  padding: 0;
+
+  border: none;
+  background: none;
+
+  width: $icon-width-mobile;
+
+  @include onDesktop() {
+    width: $icon-width-desk;
+  }
+}
+</style>
