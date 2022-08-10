1290f4da86e5662e8f360f4081a74e6b7e06f9b2
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-10 23:09:03 +0300

    Add Logo

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img
+      src="../assets/logo.svg"
+      alt="Company logo"
+      class="logo"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/styles/utils/mixins.scss";
+
+.logo {
+  width: 102px;
+
+  @include onDesktop() {
+    width: 198px;
+  }
+}
+</style>
