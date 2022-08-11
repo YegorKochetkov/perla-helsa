3defa7d08397c2eb28e2f821d9789d150f051a23
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-11 11:39:17 +0300

    Add routing for header options

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,38 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/omega-3",
+    name: "omega-3",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Omega3View.vue"),
+  },
+  {
+    path: "/vitamins",
+    name: "vitamins",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/VitaminsView.vue"),
+  },
+  {
+    path: "/food_supplement",
+    name: "food_supplement",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/FoodSupplementView.vue"),
+  },
+  {
+    path: "/delivery",
+    name: "delivery",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/DeliveryView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/Omega3View.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Omega-3 page</h1>
+  </div>
+</template>
