de7a1f125c290e1539139fd45f29959a5346be21
Yegor Kochetkov <KochetkovYegorNikolaevich@gmail.com>
2022-08-22 08:27:39 +0300

    Add kids submenu

---
+++ src/components/SideMenu/SideMenuKids.vue
@@ -0,0 +1,107 @@
+<template>
+  <nav class="nav">
+    <a
+      role="button"
+      tabindex="0"
+      href="#"
+      class="nav__title"
+      @click="toggleSubMenuKids"
+    >
+      Продукты для детей
+    </a>
+
+    <router-link
+      to="/adults"
+      class="nav__link"
+      @click="toggleMenu"
+    >
+      Все
+    </router-link>
+
+    <router-link
+      v-for="(product, index) in kidsProducts"
+      :key="product.title + index"
+      :to="'/' + product.category"
+      class="nav__link"
+      @click="toggleMenu"
+    >
+      {{ product.title }}
+    </router-link>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import useProductsStore from "@/stores/products";
+import useMenuStore from "@/stores/menu";
+
+const { kidsProducts } = useProductsStore();
+const { toggleSubMenuKids, toggleMenu } = useMenuStore();
+</script>
+
+
+<style scoped lang="scss">
+@import "@/styles/utils/typography.scss";
+@import "@/styles/utils/colors.scss";
+@import "@/styles/utils/mixins.scss";
+@import "@/styles/utils/extends.scss";
+
+.nav {
+  display: flex;
+  flex-direction: column;
+  gap: $side-menu-gap;
+
+  &__title {
+    position: relative;
+
+    margin: 0;
+    padding-left: 30px;
+
+    width: max-content;
+
+    text-decoration: none;
+    color: $color-primary;
+
+    border-bottom: 1px solid transparent;
+    @include hover(border-color, $color-primary);
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &::before {
+      position: absolute;
+      bottom: 25%;
+      left: 0;
+
+      content: "";
+      display: block;
+      width: 10px;
+      height: 10px;
+
+      border-right: 1px solid $color-primary;
+      border-bottom: 1px solid $color-primary;
+
+      transform: rotate(135deg);
+    }
+  }
+
+  &__link {
+    margin: 0;
+    width: max-content;
+
+    text-decoration: none;
+    color: $color-primary;
+
+    border-bottom: 1px solid transparent;
+    @include hover(border-color, $color-primary);
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &:not(:nth-of-type(2)) {
+      font-weight: 400;
+    }
+  }
+}
+</style>
